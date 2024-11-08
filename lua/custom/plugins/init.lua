469adb99d5b5eeaa29d214ad61f2e96716999eaa
Thiago Negri <thiago.negri@babycenter.com>
2024-11-08 09:56:06 -0300

    Tab keymaps

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,9 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+vim.keymap.set('n', '<leader><tab>', '<CMD>tabn<CR>', { desc = 'Next [Tab]' })
+vim.keymap.set('n', '<leader><S-tab>', '<CMD>tabp<CR>', { desc = 'Previous [Tab]' })
+vim.keymap.set('n', '<leader><A-tab>', '<CMD>tabnew<CR>', { desc = 'New [Tab]' })
+
 return {}
