01087442bc4eed3fc53893ff36125ff3ac849901
Thiago Negri <thiago.negri@babycenter.com>
2024-11-13 12:12:33 -0300

    Add keys for quickfix navigation

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,8 +3,15 @@
 --
 -- See the kickstart.nvim README for more information
 
+-- Tabs
 vim.keymap.set('n', '<leader><tab>', '<CMD>tabn<CR>', { desc = 'Next [Tab]' })
 vim.keymap.set('n', '<leader><S-tab>', '<CMD>tabp<CR>', { desc = 'Previous [Tab]' })
 vim.keymap.set('n', '<leader><A-tab>', '<CMD>tabnew<CR>', { desc = 'New [Tab]' })
+
+-- Quickfix navigation
+vim.keymap.set('n', '<A-[>', '<CMD>:cp<CR>', { desc = 'Previous Quickfix' })
+vim.keymap.set('n', '<A-]>', '<CMD>:cn<CR>', { desc = 'Next Quickfix' })
+vim.keymap.set('n', '<A-\\>', '<CMD>:ccl<CR>', { desc = 'Close Quickfix' })
+vim.keymap.set('n', '<A-|>', '<CMD>:cope<CR>', { desc = 'Open Quickfix' })
 
 return {}
