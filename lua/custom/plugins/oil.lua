a3d356a678a05ebe5ad8d5290800eea2c3d45172
Thiago Negri <evohunz@gmail.com>
2024-11-18 22:27:31 -0300

    Use mini.icons for Oil

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -13,8 +13,8 @@ return {
         'icon',
       },
     },
-    -- dependencies = { { 'echasnovski/mini.icons', opts = {} } },
-    dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if prefer nvim-web-devicons
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    -- dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if prefer nvim-web-devicons
     init = function()
       vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
     end,
