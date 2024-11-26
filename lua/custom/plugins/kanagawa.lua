9542e025ab31661033a829aeb3c36510cd8f1bdd
Thiago Negri <evohunz@gmail.com>
2024-11-26 09:32:15 -0300

    update theme

--- lua/custom/plugins/kanagawa.lua
+++ lua/custom/plugins/kanagawa.lua
@@ -1,7 +1,35 @@
 return {
   {
     'rebelot/kanagawa.nvim',
-    opts = {},
+    opts = {
+      -- transparent = true,
+      theme = 'dragon',
+      background = {
+        dark = 'dragon',
+        light = 'lotus',
+      },
+      colors = {
+        theme = {
+          all = {
+            ui = {
+              bg_gutter = 'none',
+            },
+          },
+        },
+      },
+      overrides = function(colors)
+        local theme = colors.theme
+        return {
+          TelescopeTitle = { fg = theme.ui.special, bold = true },
+          TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+          TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+          TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+          TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+          TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+          TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+        }
+      end,
+    },
     init = function()
       vim.cmd.colorscheme 'kanagawa'
     end,
